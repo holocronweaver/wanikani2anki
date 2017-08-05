945e978d3249762da9a47300cb43d86966de0354
Jesse Johnson <holocronweaver@gmail.com>
2017-08-04 21:21:06 -0700

    Add error handling for GET request.

--- wanikani2anki.py
+++ wanikani2anki.py
@@ -3,6 +3,7 @@
 # License, v2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
 import json
+from urllib.error import *
 from urllib.request import *
 
 headers = {}
@@ -13,8 +14,15 @@ with open('apikey.txt', 'r') as f:
 rooturl = 'https://www.wanikani.com/api/v2'
 
 request = Request(rooturl + '/user', headers=headers)
-response = urlopen(request)
-print(response.getcode())
-print(response.info())
+try: response = urlopen(request)
+except URLError as error:
+    print('Error while fetching user data: ' + error.reason)
+    exit()
+# print(response.getcode())
+# print(response.info())
 user = json.loads(response.read().decode())
-print(user)
+# print(user)
+print("""Fetching information for
+  user:  {username}
+  level: {level}
+""".format(**user['data']))
