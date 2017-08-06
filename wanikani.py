735856e51944b09c0815325756c4cb9815134bb5
Jesse Johnson <holocronweaver@gmail.com>
2017-08-05 18:24:25 -0700

    First functional WaniKani to Anki exporter.
    * Plenty of work left to do.

---
+++ wanikani.py
@@ -0,0 +1,73 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+import json
+import os
+from urllib.error import *
+from urllib.request import *
+
+import genanki
+
+class WaniKani:
+    rooturl = 'https://www.wanikani.com/api/v2'
+
+    def get_json(self, request, description):
+        """Make a GET request and return resulting JSON."""
+        try: response = urlopen(request)
+        except URLError as error:
+            print('Error while fetching {}: {}'.format(description, error.reason))
+            raise error
+        # print(response.getcode())
+        # print(response.info())
+        return json.loads(response.read().decode())
+
+    def get_from_api(self, resource, url_ext, headers):
+        """Make a WaniKani API request and return the result.
+
+        Make as many GET requests as needed to obtain all the data for
+        a particular WaniKani API request, then merge results.
+        """
+        data = None
+        pages = []
+        next_url = self.rooturl + url_ext
+        while next_url and next_url != 'null':
+            request = Request(next_url, headers=headers)
+            pages.append(self.get_json(request, type))
+            if 'pages' in pages[-1]:
+                next_url = pages[-1]['pages']['next_url']
+            else:
+                next_url = None
+        data = pages[0]
+        for page in pages[1:]:
+            data['data'] += page['data']
+        return data
+
+    def get(self, resource, url_ext, headers, path, do_update=False):
+        """Retrieve cached WaniKani data or, if none, retrieve it via WaniKani API.
+        """
+        filename = path + resource + '.json'
+        if os.path.isfile(filename):
+            print(resource + ' has existing JSON cache.')
+            with open(filename, 'r') as f:
+                 data = json.load(f)
+
+            # Check for updates and update as needed.
+            if do_update:
+                url_ext_addendum = '&updated_after={}'.format(data['data_updated_at'])
+                if '?' not in url_ext: url_ext_addendum[0] = '?'
+                url_ext += url_ext_addendum
+                update = self.get_from_api(resource, url_ext, headers)
+                if int(update['total_count']) > 0:
+                    data['data'].update(update['data'])
+                    with open(filename, 'w') as f:
+                        json.dump(data, f)
+
+            data['from_cache'] = True
+        else:
+            if not os.path.isdir(path):
+                os.makedirs(path)
+            data = self.get_from_api(resource, url_ext, headers)
+            with open(filename, 'x') as f:
+                json.dump(data, f)
+            data['from_cache'] = False
+        return data
--- wanikani2anki.py
+++ wanikani2anki.py
@@ -2,27 +2,113 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v2.0. If a copy of the MPL was not distributed with this
 # file, you can obtain one at http://mozilla.org/MPL/2.0/.
-import json
-from urllib.error import *
-from urllib.request import *
+"""
+TODO: User-independent cache.
+TODO: Per-user cache.
+TODO: One model per subject.
+TODO: Consider in long term serializing Anki deck instead of WaniKani JSON.
+"""
+import yaml
+
+from wanikani import *
+
+wk = WaniKani()
 
 headers = {}
-with open('apikey.txt', 'r') as f:
-    apikey = f.readline().strip()
-    headers['Authorization'] = 'Token token=' + apikey
+apikey = ''
+if os.path.isfile('apikey.txt'):
+    with open('apikey.txt', 'r') as f:
+        apikey = f.readline().strip()
+        headers['Authorization'] = 'Token token=' + apikey
+else:
+    apikey = input('WaniKani API V2 key (not V1!): ')
 
-rooturl = 'https://www.wanikani.com/api/v2'
+try: user = wk.get_from_api('userdata', '/user', headers)
+except URLError: print('Invalid API V2 key!'); exit()
 
-request = Request(rooturl + '/user', headers=headers)
-try: response = urlopen(request)
-except URLError as error:
-    print('Error while fetching user data: ' + error.reason)
-    exit()
-# print(response.getcode())
-# print(response.info())
-user = json.loads(response.read().decode())
-# print(user)
+path = 'cache/{}/'.format(user['data']['username'])
+
+if not os.path.isfile('apikey.txt'):
+    with open('apikey.txt', 'w+') as f:
+        f.write(apikey)
+
 print("""Fetching information for
-  user:  {username}
-  level: {level}
-""".format(**user['data']))
+      user:  {username}
+      level: {level}
+    """.format(**user['data']))
+
+data = {}
+for subject in ('radical', 'kanji', 'vocabulary'):
+    data[subject] = wk.get(subject, '/subjects?type={}'.format(subject), headers, path)
+    data[subject]['data'].sort(key=lambda x: x['id'])
+
+    # Merge subjectable data into subject data to recreate unified
+    # object data of WaniKani API V1.
+    # for subjectable in ('study_materials', 'review_statistics', 'assignments'):
+    for subjectable in ('review_statistics', 'assignments'):
+        subdata = wk.get(
+            '{}-{}'.format(subject, subjectable),
+            '/{}?subject_type={}'.format(subjectable, subject), headers, path)
+        subdata['data'].sort(key=lambda x: x['data']['subject_id'])
+
+        # with open('debug.txt', 'w') as f:
+        #     for item in subdata['data']:
+        #         f.write(str(item['data']['subject_id']) + '\n')
+        #     # for item in data[subject]['data']:
+        #     #     f.write(str(item['id']) + '\n')
+
+        datumiter = iter(data[subject]['data'])
+        datum = next(datumiter)
+        for subdatum in subdata['data']:
+            while datum['id'] != subdatum['data']['subject_id']:
+                try: datum = next(datumiter)
+                except StopIteration: print('Error: Could not find subject id {}. Aborting.'.format(subdatum['data']['subject_id']))
+
+                # msg = """id of subject '{}' not equal to\n\tsubject_id of subjectable '{}'. Aborting!""".format(subject, subjectable)
+                # print(data[subject]['data'][i])
+                # print(subdata['data'][i]['data'])
+                # raise Exception(msg)
+            datum['data'].update(subdatum['data'])
+
+print(data['vocabulary']['data'][0])
+
+with open('cards.yaml', 'r') as f:
+    cards = f.read()
+    cards = yaml.load(cards)
+with open('wanikani.css','r') as f:
+    css = f.read()
+
+model = genanki.Model(
+    1108933879,
+    'WaniKani',
+    fields=cards['fields'],
+    templates=cards['templates'],
+    css=css)
+
+deck = genanki.Deck(
+    1970043342,
+    'WaniKani')
+
+filename = path + 'WaniKani.apkg'
+if os.path.isfile(filename):
+    os.remove(filename)
+
+for subject in ('radical', 'kanji', 'vocabulary'):
+    characters_key = 'character' if subject != 'vocabulary' else 'characters'
+    for datum in data[subject]['data']:
+        try:
+            characters = datum['data'][characters_key]
+            if not characters: characters = 'FIXME'
+            meaning = next(m['meaning'] for m in datum['data']['meanings'] if m['primary'])
+            if subject != 'radical':
+                reading = next(m['reading'] for m in datum['data']['readings'] if m['primary'])
+            else:
+                reading = 'none'
+        except Exception:
+            print(datum['data'])
+            raise
+        fields = [characters, meaning, reading]
+        note = genanki.Note(model=model, fields=fields)
+        deck.add_note(note)
+
+genanki.Package(deck).write_to_file(filename)
