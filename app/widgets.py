8431768aa0fdaaa8a615512bab9b53a0275dcbe5
Jesse Johnson <holocronweaver@gmail.com>
2017-08-29 20:22:21 -0700

    Clarify licenses.

--- app/widgets.py
+++ app/widgets.py
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
 from kivy.base import EventLoop
 from kivy.graphics import Color, Rectangle
 from kivy.uix.button import Button
